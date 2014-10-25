9ec1fa02acd58fafb91389c639967a6bcc3653ba
yomin.cheng <yomin.cheng@gmail.com>
2014-10-25 14:26:30 +0800

    vendor
    Signed-off-by: yomin.cheng <yomin.cheng@gmail.com>

--- environ/echoarg.c
+++ environ/echoarg.c
@@ -1,10 +1,12 @@
 #include "apue.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *argp[])
 {
     int i;
 
     for (i = 0; i < argc; i++)  /* echo all command-line args */
         printf("argv[%d]: %s\n", i, argv[i]);
+    for (i = 0; i < 10; i++)  /* echo all command-line args */
+    printf("argp[%d]: %s\n", i, argp[i]);
     exit(0);
 }
